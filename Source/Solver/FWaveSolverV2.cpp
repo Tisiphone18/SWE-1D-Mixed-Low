02af5280f25bcd5a4561d9dbb918889147aa876d
lenaholtmannspoetter <lena@holtmannspoetter.de>
2024-10-23 11:15:07 +0200

    added some TODOS to look at

--- Source/Solver/FWaveSolverV2.cpp
+++ Source/Solver/FWaveSolverV2.cpp
@@ -1,7 +1,8 @@
 #include "FWaveSolverV2.hpp"
-
 #include <cmath>
 #include <iostream>
+#include <cassert>
+
 
 void Solvers::FWaveSolverStudentV2::computeNetUpdates(
 const RealType& hL, const RealType& hR,
@@ -14,9 +15,13 @@ RealType& huNetUpdateRight,
 RealType& maxEdgeSpeed)
 {
 
-  // Physical constant
-  const RealType g = 9.81; // gravity
+  // Physical constant: gravity
+  const RealType g = 9.81; // in (m/s^2)
 
+  // Compute velocity for left and right sides
+  //TODO : assert h > 0 ?
+  //assert(hL > 0 && "hL must be greater than zero to avoid division by zero.");
+  //assert(hR > 0 && "hR must be greater than zero to avoid division by zero.");
   RealType uL = huL/hL;
   RealType uR = huR/hR;
 
@@ -25,12 +30,16 @@ RealType& maxEdgeSpeed)
   RealType uRoe = uL*std::sqrt(hL)+uR*std::sqrt(hR);
   uRoe /= std::sqrt(hL)+std::sqrt(hR);
 
+  // Compute Roe eigenvalues
   RealType x = std::sqrt(g*hRoe);
   RealType lambda1 = uRoe - x;
   RealType lambda2 = uRoe + x;
 
+  // Compute flux differences (delta F)
   RealType halfG = 0.5*g;
   RealType deltaF[2] = {huR - huL, hR*uR*uR+halfG*hR*hR-hL*uL*uL-halfG*hL*hL};
+  //TODO : assert lambda1 != lambda2
+  //assert(lambda1 != lambda2 && "The values of the Roe eigenvalues must be different to avoid division by zero.");
   RealType inverse_mul = 1/(lambda2 - lambda1);
 
   RealType alpha1 = lambda2*deltaF[0]-deltaF[1];
@@ -38,15 +47,38 @@ RealType& maxEdgeSpeed)
   RealType alpha2 = -lambda1*deltaF[0]+deltaF[1];
   alpha2 *= inverse_mul;
 
+  // Compute net updates for height and momentum on the left side
+  // TODO : können wir vlt vereinfachen zu :
+  //  checke ob VZ unterschiedlich von lambda 1 und lambda 2
+  //  -> falls ja so :
+  // if (lambda1 < 0) {
+  //    hNetUpdateLeft += alpha1;
+  //    huNetUpdateLeft += lambda1 * alpha1;
+  //}
+  // ...
+  // TODO : falls VZ gleich ????? -> Wie weiter ?
+
   hNetUpdateLeft = lambda1 < 0?alpha1:0;
   hNetUpdateLeft += lambda2 < 0?alpha2:0;
   huNetUpdateLeft = lambda1 < 0?lambda1*alpha1:0;
   huNetUpdateLeft += lambda2 < 0?lambda2*alpha2:0;
 
+  // Compute net updates for height and momentum on the right side
   hNetUpdateRight = lambda1 > 0?alpha1:0;
   // std::cout << "lambda" << lambda1 << ":" << lambda2 << ":" << alpha1 << std::endl;
   hNetUpdateRight += lambda2 > 0?alpha2:0;
   huNetUpdateRight = lambda1 > 0?lambda1*alpha1:0;
   huNetUpdateRight += lambda2 > 0?lambda2*alpha2:0;
+
+  // Compute the maximum speed
   maxEdgeSpeed = std::fmax(std::abs(lambda1), std::abs(lambda2));
+
+  //TODO : CFL condition ? Stabilität im 1D : C = (u * deltaT) / (deltaX) muss kleiner als 1 sein (deltaT ≙ Zeitschritt, deltaX ≙ Ortsschritt)
+  // => u * deltaT / deltaX < CFL => u * deltaT < CFL * deltaX => deltaT < (CFL * deltaX) / u mit u ≙ max(lambda1, lambda2)
+  // => braucht zwei zusätliche Params !
+  // CFL Zahl <= 1, save value 0.5
+  // assert(deltaT < (CFL * deltaX / lambdaMax) && "The time step size or the mesh step size violate the CFL condition, causing potential instability.");
+  //vgl. https://en.wikipedia.org/wiki/Courant–Friedrichs–Lewy_condition
+  //vgl. https://www.simscale.com/blog/cfl-condition/
+  //TODO : gibts noch mehr asserts ?
 }
--- Source/Solver/FWaveSolverV2.hpp
+++ Source/Solver/FWaveSolverV2.hpp
@@ -6,6 +6,26 @@
 namespace Solvers {
   class FWaveSolverStudentV2 {
   public:
+
+/**
+ * @brief Implementation of the F-Wave Solver.
+ *
+ * This function calculates the net updates for the height and momentum
+ * on the left and right sides of the edge using the F-Wave approach.
+ *
+ * @param[in] hL Water height on the left side of the edge.
+ * @param[in] hR Water height on the right side of the edge.
+ * @param[in] huL Water momentum on the left side of the edge.
+ * @param[in] huR Water momentum on the right side of the edge.
+ * @param[in] bL Bathymetry value on the left side of the edge.
+ * @param[in] bR Bathymetry value on the right side of the edge.
+ * @param[out] hNetUpdateLeft Net update for water height to the left cell.
+ * @param[out] hNetUpdateRight Net update for water height to the right cell.
+ * @param[out] huNetUpdateLeft Net update for momentum to the left cell.
+ * @param[out] huNetUpdateRight Net update for momentum to the right cell.
+ * @param[out] maxEdgeSpeed The maximum wave speed across the edge.
+ */
+
     void computeNetUpdates(
       const RealType &hL, const RealType &hR,
       const RealType &huL, const RealType &huR,
