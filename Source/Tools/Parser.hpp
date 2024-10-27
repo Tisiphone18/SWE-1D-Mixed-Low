1e0421cfee4bd4c77b06161de80bd5c07d2b4649
lenaholtmannspoetter <lena@holtmannspoetter.de>
2024-10-27 17:25:31 +0100

    Parser in Tools - In the middle_states.csv you need to delete the first lines (with no values) for it to woek

---
+++ Source/Tools/Parser.hpp
@@ -0,0 +1,49 @@
+/**
+* @file
+*  CSV Parser, part of our own SWE1D tools
+*/
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "RealType.hpp"
+
+namespace Tools {
+
+ class Parser {
+ public:
+   enum LogLevel { INFO, ERROR };
+
+ private:
+   /** Input file stream for the CSV file */
+   std::ifstream inputFile_;
+
+ private:
+   Parser();
+
+ public:
+   static Parser parser;
+
+   ~Parser() = default;
+
+   void openFile(const std::string& filePath);
+   void closeFile();
+
+   /**
+        * Parses the CSV file line by line.
+        * Each line must have 5 values.
+        * returns the values of n files
+        * skips the first s values
+    */
+   std::vector<std::vector<RealType>> parseFile(int n, int s);
+
+ private:
+   void printValues(const std::vector<double>& values);
+   void log(const std::string& message, LogLevel level);
+ };
+
+} // namespace Tools
