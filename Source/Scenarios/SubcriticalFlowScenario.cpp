409d946fa45eaab6f6070b7c85d791e892d3aee1
JonathanPins <jpins05@gmail.com>
2024-11-02 16:45:21 +0100

    Implemented ToDo's / Fix mistake

--- Source/Scenarios/SubcriticalFlowScenario.cpp
+++ Source/Scenarios/SubcriticalFlowScenario.cpp
@@ -15,23 +15,21 @@ Scenarios::SubcriticalFlowScenario::SubcriticalFlowScenario(unsigned int size):
 RealType Scenarios::SubcriticalFlowScenario::getCellSize() const { return 25.0 / size_; } //TODO RealType(25.0)?
 
 RealType Scenarios::SubcriticalFlowScenario::getHeight(unsigned int pos) const {
-    // TODO assert x \el [0,size_]? imo the user should be aware of size_, as this is passed onto the constructor
   return -getBathymetry(pos);
 }
 
 RealType Scenarios::SubcriticalFlowScenario::getMomentum(unsigned int pos) const {
-  // TODO assert x \el [0,size_]? imo the user should be aware of size_, as this is passed onto the constructor
   return 4.42;
 }
 
 RealType Scenarios::SubcriticalFlowScenario::getBathymetry(unsigned int pos) const {
-  if (pos <= size_ * 8 / 25 || pos >= size_ * 12 / 25) { // TODO this is wrong, size_ != spacial domain!: pos <= size_ * 8 / 25 || pos >= size_ * 12 / 25
+  if (pos <= size_ * 8 / 25 || pos >= size_ * 12 / 25) { 
     return -2;
   }
 
   //erstmal Zwischenschritte berechnen, da es davor zu Fehlern kam
   // können wir noch zusammensetzen 
-  double p = (pos - 10) * (pos - 10); // TODO this is wrong as well; convert pos to spacial coordinate: RealType(pos) / size_ * 25.0 - 10.0
+  double p = (RealType(pos) / size_ * 25.0 - 10.0) * (RealType(pos) / size_ * 25.0 - 10.0);
   double p_ = (0.05 * p);
   return -1.8 - p_;
 }
